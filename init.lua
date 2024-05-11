ccdacf08288c619aa87a88f65b259b0cb683f819
Nojoponken <rydennoah@gmail.com>
2024-05-12 00:04:09 +0200

    lualine + neotree + other small changes

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+
 -- Lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -22,12 +24,13 @@ vim.cmd("set number")
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+vim.cmd("set shiftwidth=0")
 
 -- Vim keybinds
-vim.g.mapleader = " "
-
 vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<C-h>", "<cmd>cd $HOMEPATH<cr>", {})
+vim.keymap.set("n", "<leader>win", "<cmd>cd $HOMEPATH/Appdata/Local/nvim<cr>", {}) -- Windows get to config
+
+-- Plugin keybinds
 vim.keymap.set("n", "<leader>g", "<cmd>Neogit<cr>", {})
+vim.keymap.set("n", "<leader>t", "<cmd>Neotree filesystem reveal left<cr>", {})
